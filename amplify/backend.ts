9675c07dcd4ce4b8a7fd6cd16296a125762eccd2
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-11 10:28:35 +0000

    updated imports to include text-to-speech-function in backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { myFirstFunction } from './my-first-function/resource';
+import { myFirstFunction } from './functions/my-first-function/resource';
+import { textToSpeechFunction } from './functions/text-to-speech-function/resource';
 
 defineBackend({
   auth,
   data,
   storage,
-  myFirstFunction
+  myFirstFunction,
+  textToSpeechFunction
 });
