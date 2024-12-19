62d6ca12e478add48504be3907c8cb8248c71427
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-19 21:14:01 +0000

    I'M SO DONE  ;-;

--- amplify/functions/text-to-speech-function/resource.ts
+++ amplify/functions/text-to-speech-function/resource.ts
@@ -1,9 +1,11 @@
 import { defineFunction, secret } from "@aws-amplify/backend";
+import { APIGATEWAY_AUTHORIZER_CHANGE_DEPLOYMENT_LOGICAL_ID } from "aws-cdk-lib/cx-api";
 
 export const textToSpeechFunction = defineFunction({
   name: "text-to-speech-function",
   entry: "./handler.ts",
   environment: {
+    ELEVENLABS_API_KEY: secret("	ELEVENLABS_API_KEY"),
     API_KEY: secret("	ELEVENLABS_API_KEY"),
     S3_BUCKET_NAME: "amplify-d3vjzia12splxy-dev-bra-audiobucket33d52ccb-epii5m1e0byr",
   },
