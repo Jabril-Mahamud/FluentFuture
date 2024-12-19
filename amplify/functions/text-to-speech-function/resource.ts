ffbeb4a1642dc60573234906c0a6d294f8ed3a57
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-19 17:50:14 +0000

    I think I fixed the api key issue lmao

--- amplify/functions/text-to-speech-function/resource.ts
+++ amplify/functions/text-to-speech-function/resource.ts
@@ -1,4 +1,5 @@
 import { defineFunction, secret } from "@aws-amplify/backend";
+import { env } from '$amplify/env/textToSpeechFunction'; // the import is '$amplify/env/<function-name>'
 
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY as string;
 
@@ -7,7 +8,7 @@ export const textToSpeechFunction = defineFunction({
   name: "text-to-speech-function",
   entry: "./handler.ts",
   environment: {
-    ELEVENLABS_API_KEY: secret(ELEVENLABS_API_KEY),
+    ELEVENLABS_API_KEY: secret("ELEVENLABS_API_KEY"),
     BUCKET_NAME: "audio",
     S3_BUCKET_NAME: "amplify-d3vjzia12splxy-dev-bra-audiobucket33d52ccb-epii5m1e0byr",
   },
