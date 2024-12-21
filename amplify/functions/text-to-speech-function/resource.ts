5907917618379642a08a80ab1d7ce64e7007cde9
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2024-12-21 16:52:40 +0000

    temporary test, will delete

--- amplify/functions/text-to-speech-function/resource.ts
+++ amplify/functions/text-to-speech-function/resource.ts
@@ -4,7 +4,7 @@ export const textToSpeechFunction = defineFunction({
   name: "text-to-speech-function",
   entry: "./handler.ts",
   environment: {
-    ELEVENLABS_API_KEY: secret('ELEVENLABS_API_KEY'),
+    ELEVENLABS_API_KEY: "sk_61819115279a7fc283007b9189d203a74d5a850bc42282b6",
     S3_BUCKET_NAME: "amplify-d3vjzia12splxy-dev-bra-audiobucket33d52ccb-epii5m1e0byr",
   }
 });
